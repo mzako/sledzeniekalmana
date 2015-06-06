aa1fe23cdd4de31d88206ea2439fb97e0ce55877
mostua <mostua@gmail.com>
2015-06-06 10:18:22 +0200

    filtr dziala

--- src/filter_app/main.cpp
+++ src/filter_app/main.cpp
@@ -12,7 +12,7 @@ using namespace filter_app;
 using namespace network;
 
 void start_filter(std::shared_ptr<filter_module> filter_module_, std::shared_ptr<blocking_queue> b_queue, std::shared_ptr<sending_buffer> sending_buffer) {
-    //filter_module_->run(b_queue, sending_buffer);
+    filter_module_->run(b_queue, sending_buffer);
 }
 
 int main(int argc, char ** argv){
@@ -24,19 +24,19 @@ int main(int argc, char ** argv){
     else
     {
         std::shared_ptr<sending_buffer> filter_sending_buf(new sending_buffer);
-        //server filter_server(std::atoi(argv[3]), filter_sending_buf);
-        //boost::thread filter_server_thread(boost::bind(&server::operator(), &filter_server));
+        server filter_server(std::atoi(argv[3]), filter_sending_buf);
+        boost::thread filter_server_thread(boost::bind(&server::operator(), &filter_server));
 
-        std::shared_ptr<blocking_queue> b_queue;
+        std::shared_ptr<blocking_queue> b_queue(new blocking_queue);
         client filter_client(argv[1], argv[2], b_queue);
-        //boost::thread client_thread(filter_client);
+        boost::thread client_thread(filter_client);
 
-        //boost::thread filter_main_thread(boost::bind(start_filter, filter_module::get_instance(), b_queue, filter_sending_buf));
-        //client_thread.join();
-        //filter_module::get_instance()->stop(b_queue);
-        //filter_server.stop();
-        //filter_server_thread.join();
-        //filter_main_thread.join();
+        boost::thread filter_main_thread(boost::bind(start_filter, filter_module::get_instance(), b_queue, filter_sending_buf));
+        client_thread.join();
+        filter_module::get_instance()->stop(b_queue);
+        filter_server.stop();
+        filter_server_thread.join();
+        filter_main_thread.join();
 
     }
 
