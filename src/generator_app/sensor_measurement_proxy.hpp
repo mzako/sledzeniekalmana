57527d7a7dd82d101049d2c2af927910ef311d0c
mostua <mostua@gmail.com>
2015-06-11 15:39:08 +0200

    proxies

--- src/generator_app/sensor_measurement_proxy.hpp
+++ src/generator_app/sensor_measurement_proxy.hpp
@@ -1,8 +1,7 @@
 /*
- * sensor_measurement_proxy.hpp
- *
- *  Created on: Jun 3, 2015
- *      Author: elohhim
+ *  \file sensor_measurement_proxy.hpp
+ *  \details   This file contains sensor_measurement_proxy class
+ *  \author    Jan Kumor
  */
 
 #ifndef SENSOR_MEASUREMENT_PROXY_HPP
@@ -16,14 +15,35 @@
 
 namespace generator_app {
 
+
+/**
+ * \brief Proxy allows load and save sensors measurement positions from JSON format
+ *
+ * Proxy which allows to easy serialize/deserialize measurements
+ *
+ */
 class sensor_measurement_proxy {
 public:
+    /**
+     * \brief Set sensor observer object
+     *
+     * Set sensor observer object from where measurement positions will be be serialized
+     *
+     * \param real smart pointer to sensor observer object
+     */
     sensor_measurement_proxy( std::shared_ptr<sensor_observer> real) : real_(real) {};
 
+    /**
+     * \brief load method needed by Cereal to deserialize measurements
+     */
     template<class Archive>
     void load(Archive& archive) {
         std::cout << "Loading sensor_measurement_proxy is illegal." << std::endl;
     }
+
+    /**
+     * \brief save method needed by Cereal to serialize measurements
+     */
     template<class Archive>
     void save(Archive& archive) const {
         archive(
--- src/generator_app/sensor_position_proxy.hpp
+++ src/generator_app/sensor_position_proxy.hpp
@@ -1,29 +1,48 @@
 /*
- * sensor_position_proxy.hpp
- *
- *  Created on: Jun 3, 2015
- *      Author: elohhim
+ *  \file sensor_position_proxy.hpp
+ *  \details   This file contains sensor_position_proxy class
+ *  \author    Jan Kumor
  */
+
 
 #ifndef SENSOR_POSITION_PROXY_HPP
 #define SENSOR_POSITION_PROXY_HPP
 
 #include <memory>
-
 #include <cereal/cereal.hpp>
-
 #include "sensor_observer.hpp"
 
 namespace generator_app {
 
+
+/**
+ * \brief Proxy allows load and save real sensors positions from JSON format
+ *
+ * Proxy which allows to easy serialize/deserialize positions
+ *
+ */
 class sensor_position_proxy {
 public:
+    /**
+     * \brief Set sensor observer object
+     *
+     * Set sensor observer object from where real positions will be be serialized
+     *
+     * \param real smart pointer to sensor observer object
+     */
     sensor_position_proxy( std::shared_ptr<sensor_observer> real) : real_(real) {};
 
+    /**
+     * \brief load method needed by Cereal to deserialize real positons
+     */
     template<class Archive>
     void load(Archive& archive) {
         std::cout << "Loading sensor_position_proxy is illegal." << std::endl;
     }
+
+    /**
+     * \brief load method needed by Cereal to serialize real positons
+     */
     template<class Archive>
     void save(Archive& archive) const {
         archive (
