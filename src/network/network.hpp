4651a35b8a763a90907d7364d59a477f4a3701e0
elohhim <elohhim@gmail.com>
2015-06-12 21:13:58 +0200

    network module doxy

---
+++ src/network/network.hpp
@@ -0,0 +1,9 @@
+/**
+ * \defgroup NetworkLibrary
+ * \brief Provides network classes used for communication between modules
+ *
+ * Static library providing:
+ * - TCP client
+ * - TCP server
+ *
+ */
